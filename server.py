7036fcb2793b82558dac835119ab63161d726cee
Enrico Polesel <enrico.polesel@sns.it>
2016-04-09 16:09:37 +0200

    server_id check

--- server.py
+++ server.py
@@ -7,6 +7,9 @@ netopt = {'client_listen_port':"68",
           'server_listen_port':"67",
           'listen_address':"0.0.0.0"}
 
+goodservers_str = [ '192.168.251.2', '192.168.251.3' ]
+goodservers = [ [ int(n) for n in s.split('.') ] for s in goodservers_str ] 
+
 class Server(DhcpServer):
     def __init__(self, options):
         DhcpServer.__init__(self,options["listen_address"],
@@ -14,19 +17,25 @@ class Server(DhcpServer):
                             options["server_listen_port"])
         
     def HandleDhcpDiscover(self, packet):
-        print packet.str()
+        pass
 
     def HandleDhcpRequest(self, packet):
-        print packet.str()
+        server_identifier = packet.GetOption('server_identifier')
+        if server_identifier is not None:
+            if server_identifier is not []:
+                if server_identifier not in goodserver:
+                    with open('badpackets.txt','a') as f:
+                        f.writeline(packet.str())
+                    print packet.str()
 
     def HandleDhcpDecline(self, packet):
-        print packet.str()
+        pass
 
     def HandleDhcpRelease(self, packet):
-        print packet.str()
+        pass
 
     def HandleDhcpInform(self, packet):
-        print packet.str()
+        pass
 
 
 def main():
