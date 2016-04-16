02078f484b8ea9bf73b8090637daf010f2acc9cf
Enrico Polesel <saggiopol@gmail.com>
2016-04-16 10:20:38 +0200

    refining dhcp discover message in the trigger

--- rouguedhcptrigger.py
+++ rouguedhcptrigger.py
@@ -13,5 +13,5 @@ conf.checkIPaddr = False
 fam,hw = get_if_raw_hwaddr(conf.iface)
 
 for i in [ 20,253,8]:
-    pkt = Ether(dst="ff:ff:ff:ff:ff:ff",src=hw)/Dot1Q(vlan=i)/IP(src="0.0.0.0",dst="255.255.255.255",ttl=1)/UDP(sport=68,dport=67)/BOOTP(chaddr=hw,xid=random.randint(0, 0xFFFFFFFF))/DHCP(options=[("message-type","discover"),"end"])
+    pkt = Ether(dst="ff:ff:ff:ff:ff:ff",src=hw)/Dot1Q(vlan=i)/IP(src="0.0.0.0",dst="255.255.255.255",ttl=1)/UDP(sport=68,dport=67)/BOOTP(chaddr=hw,xid=random.randint(0, 0xFFFFFFFF),flags=1<<15)/DHCP(options=[("message-type",1),"end"])
     sendp(pkt)
