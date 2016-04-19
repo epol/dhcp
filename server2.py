8217db7482721b30cbe49390778ba8b8bb10972f
Enrico Polesel <saggiopol@gmail.com>
2016-04-19 11:39:13 +0200

    bad request cap file

--- server2.py
+++ server2.py
@@ -7,7 +7,8 @@ import commondis
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.bind(("", 67))
 
-good_servers = ['192.168.251.2']
+good_servers = ['192.168.251.2', '192.168.251.3']
+pkts = []
 
 while 1:
     data, addr = s.recvfrom(1600)
@@ -15,6 +16,8 @@ while 1:
     data = commondis.get_dhcp_infos(p)
     if data['message-type'] == 3: #DHCPREQUEST
         if data['server_id'] is not None and data['server_id'] not in good_servers:
+            pkts.append(p)
+            wrpcap("bad_request.cap",pkts)
             print("From gateway {giaddr} request for the address {requested_addr} and server_id {server_id}\n".format(**data))
 
 s.close()
