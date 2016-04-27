2d86d6b23f09909d7e4c92c5e0886e514cc30e8d
Enrico Polesel <epol@autistici.org>
2016-04-27 11:43:06 +0200

    vutuf_web: some 404

--- vutuf_web.py
+++ vutuf_web.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,abort
 
 from vutuf_base import Server,Packet,session
 
@@ -8,26 +8,34 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/server')
+@app.route('/server/')
 def server_list():
     servers = session.query(Server).all()
     return render_template('server_list.html',servers=servers)
 
-@app.route('/server/<servername>')
-def server(servername):
-    server = session.query(Server).filter(Server.name==servername).one()
-    packets = session.query(Packet).filter(Packet.server_id==server.id).order_by(Packet.date.desc()).all()
-    return render_template('server.html',server=server,packets=packets)
+@app.route('/server/<serverip>')
+def server(serverip):
+    try:
+        server = session.query(Server).filter(Server.ip==serverip).one()
+    except:
+        abort(404)
+    else:
+        packets = session.query(Packet).filter(Packet.server_id==server.id).order_by(Packet.date.desc()).all()
+        return render_template('server.html',server=server,packets=packets)
 
-@app.route('/packet')
+@app.route('/packet/')
 def packet_list():
     packets = session.query(Packet).order_by(Packet.date.desc()).all()
     return render_template('packet_list.html',packets=packets)
 
 @app.route('/packet/<int:id>')
 def packet(id):
-    packet = session.query(Packet).filter(Packet.id==id).one()
-    return render_template('packet.html',packet=packet)
+    try:
+        packet = session.query(Packet).filter(Packet.id==id).one()
+    except:
+        abort(404)
+    else:
+        return render_template('packet.html',packet=packet)
 
 if __name__ == '__main__':
     app.debug = True
